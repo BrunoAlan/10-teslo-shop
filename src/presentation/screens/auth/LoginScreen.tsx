007ba567ce4e31faa563324a704085487120f8a2
BrunoAlan <alan.brunoab@gmail.com>
2024-08-05 20:42:55 -0300

    feat: Update LoginScreen component with UI improvements
    The LoginScreen component has been updated to include UI improvements such as using the UI components from the '@ui-kitten/components' library, adding a ScrollView for better content visibility, and styling enhancements. This update enhances the user experience and aligns with the recent changes made to the UI components in the repository.

--- src/presentation/screens/auth/LoginScreen.tsx
+++ src/presentation/screens/auth/LoginScreen.tsx
@@ -1,10 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
+import { ScrollView, useWindowDimensions } from 'react-native';
+
 const LoginScreen = () => {
+  const { height } = useWindowDimensions();
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <Layout style={{ flex: 1 }}>
+      <ScrollView style={{ marginHorizontal: 40 }}>
+        <Layout style={{ paddingTop: height * 0.35 }}>
+          <Text category='h1'>Login</Text>
+          <Text category='p2'>Please login to continue</Text>
+
+          <Layout style={{ marginTop: 20 }}>
+            <Input
+              placeholder='Email'
+              style={{ marginBottom: 10 }}
+              autoCapitalize='none'
+              keyboardType='email-address'
+            />
+            <Input
+              placeholder='Password'
+              style={{ marginBottom: 10 }}
+              autoCapitalize='none'
+              secureTextEntry
+            />
+            <Layout style={{ height: 20 }} />
+            <Layout>
+              <Button onPress={() => {}}>Log in</Button>
+            </Layout>
+
+            <Layout
+              style={{
+                marginTop: 50,
+                alignItems: 'flex-end',
+                justifyContent: 'center',
+                flexDirection: 'row',
+              }}
+            >
+              <Text>Don't have an account?</Text>
+              <Text status='primary' category='s1' onPress={() => {}}>
+                {' '}
+                Sign up{' '}
+              </Text>
+            </Layout>
+          </Layout>
+        </Layout>
+      </ScrollView>
+    </Layout>
   );
 };
 export default LoginScreen;
-const styles = StyleSheet.create({});
