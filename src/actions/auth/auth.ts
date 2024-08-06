c8cbfaf7ad6b7e4cb509e30dda2d02ce19b6f427
BrunoAlan <86273936+bruno-alan@users.noreply.github.com>
2024-08-06 19:30:56 -0300

    feat: Add authLogin function for user authentication
    The `authLogin` function is added to the `auth.ts` file in the `actions/auth` directory. This function handles the login process by making a POST request to the `/auth/login` endpoint with the provided email and password. It returns the user token and information if the login is successful. This addition enhances the authentication functionality of the application.

---
+++ src/actions/auth/auth.ts
@@ -0,0 +1,32 @@
+import { tesloApi } from '@/src/config/api/tesloApi';
+import type { User } from '@/src/domain/entities/user';
+import type { AuthResponse } from '@/src/infrastructure/interfaces/auth.responses';
+
+const returnUserToken = (data: AuthResponse) => {
+  const user: User = {
+    id: data.id,
+    email: data.email,
+    fullName: data.fullName,
+    isActive: data.isActive,
+    roles: data.roles,
+  };
+
+  return {
+    user: user,
+    token: data.token,
+  };
+};
+
+export const authLogin = async (email: string, password: string) => {
+  email = email.toLowerCase();
+  try {
+    const { data } = await tesloApi.post<AuthResponse>('/auth/login', {
+      email,
+      password,
+    });
+    return returnUserToken(data);
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
