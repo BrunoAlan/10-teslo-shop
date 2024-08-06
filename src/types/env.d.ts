8ed9409c28df0de5ad9996248c5b0eb5e7af6741
BrunoAlan <86273936+bruno-alan@users.noreply.github.com>
2024-08-06 19:18:10 -0300

    Update EXPO_PUBLIC_STAGE_URL to EXPO_PUBLIC_STAGE in env.d.ts

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,5 @@
 declare module '@env' {
-  export const EXPO_PUBLIC_STAGE_URL: string;
+  export const EXPO_PUBLIC_STAGE: string;
   export const EXPO_PUBLIC_API_URL: string;
   export const EXPO_PUBLIC_API_URL_IOS: string;
   export const EXPO_PUBLIC_API_URL_ANDROID: string;
