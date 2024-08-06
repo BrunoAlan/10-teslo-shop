8e55771f84f0f786187c2238ff0b0f864b392458
BrunoAlan <86273936+bruno-alan@users.noreply.github.com>
2024-08-06 20:44:00 -0300

    refactor: Remove unused Loading component
    The Loading component in src/app/Loading.tsx is no longer used and has been deleted. This refactor removes unnecessary code and improves code cleanliness.

--- src/app/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-const Loading = () => {
-  return (
-    <View>
-      <Text>Loading</Text>
-    </View>
-  );
-};
-export default Loading;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,6 @@
-import { Button, Text, Layout } from '@ui-kitten/components';
-import { Link } from 'expo-router';
-
-export default function Index() {
-  return (
-    <Layout
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text category='h3'>Edit app/index.tsx to edit this screen.</Text>
-      <Link asChild href={'/Login'}>
-        <Button>Click me</Button>
-      </Link>
-    </Layout>
-  );
-}
+import { Text, View } from 'react-native';
+import LoadingScreen from '../presentation/screens/loading/LoadingScreen';
+const Loading = () => {
+  return <LoadingScreen />;
+};
+export default Loading;
