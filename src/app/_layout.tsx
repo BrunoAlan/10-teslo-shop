b65401ee83c62578bc11fb48de8ec19be56d7616
BrunoAlan <alan.brunoab@gmail.com>
2024-08-06 08:35:23 -0300

    feat: Update _layout component with background color customization

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,10 +3,17 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { useColorScheme } from 'react-native';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as SystemUI from 'expo-system-ui';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? eva.dark : eva.light;
+  const background =
+    colorScheme === 'dark'
+      ? theme['color-basic-800']
+      : theme['color-basic-800'];
+  SystemUI.setBackgroundColorAsync(background);
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
