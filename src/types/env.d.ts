5c44b8417ded9b6b4552b3bc3990edbc1c01394d
BrunoAlan <alan.brunoab@gmail.com>
2024-08-06 08:53:18 -0300

    feat: Add .env.template file and update .gitignore
    This commit adds a new .env.template file to the repository, which includes placeholders for environment variables related to the Expo public URLs. Additionally, the .gitignore file is updated to include the .env file, ensuring that sensitive environment-specific information is not committed to the repository. These changes improve the organization and security of the project.

---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const EXPO_PUBLIC_STAGE_URL: string;
+  export const EXPO_PUBLIC_API_URL: string;
+  export const EXPO_PUBLIC_API_URL_IOS: string;
+  export const EXPO_PUBLIC_API_URL_ANDROID: string;
+}
