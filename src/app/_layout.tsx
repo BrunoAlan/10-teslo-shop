c35efd4a81ac52f4986386980b94fb56da52db83
BrunoAlan <86273936+bruno-alan@users.noreply.github.com>
2024-08-07 09:05:46 -0300

    refactor: Update AuthProvider to handle authentication status and navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { useColorScheme } from 'react-native';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as SystemUI from 'expo-system-ui';
+import { AuthProvider } from '../presentation/providers/AuthProvider';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -16,16 +17,18 @@ export default function RootLayout() {
 
   return (
     <>
-      <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={theme}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name='index' />
-        </Stack>
-      </ApplicationProvider>
+      <AuthProvider>
+        <IconRegistry icons={EvaIconsPack} />
+        <ApplicationProvider {...eva} theme={theme}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name='index' />
+          </Stack>
+        </ApplicationProvider>
+      </AuthProvider>
     </>
   );
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Layout, Text } from '@ui-kitten/components';
+import CustomIcon from '../../components/ui/CustomIcon';
+import { useAuthStore } from '../../store/auth/useAuthStore';
+
 const HomeScreen = () => {
+  const { logout } = useAuthStore();
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text category='h1'>Home</Text>
+      <Button
+        accessoryLeft={<CustomIcon name='log-out-outline' white />}
+        onPress={logout}
+      >
+        Logout
+      </Button>
+    </Layout>
   );
 };
 export default HomeScreen;
-const styles = StyleSheet.create({});
