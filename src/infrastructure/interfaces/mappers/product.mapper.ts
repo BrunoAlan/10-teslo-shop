6c7b13486dfa2f562d5a2d9f2a1d9743d4c60d4c
BrunoAlan <alan.brunoab@gmail.com>
2024-08-07 18:53:20 -0300

    feat: Add ProductMapper class for mapping TesloProduct to Product
    This commit adds the ProductMapper class to the src/infrastructure/interfaces/mappers/product.mapper.ts file. The ProductMapper class provides a static method, `testloProductToEntity`, which maps a TesloProduct object to a Product object. This addition enhances the codebase by providing a centralized and reusable way to convert TesloProduct objects to Product objects.

---
+++ src/infrastructure/interfaces/mappers/product.mapper.ts
@@ -0,0 +1,22 @@
+import { Product } from '@/src/domain/entities/product';
+import { TesloProduct } from '../teslo-products.response';
+import { API_URL } from '@/src/config/api/tesloApi';
+
+export class ProductMapper {
+  static testloProductToEntity(tesloProduct: TesloProduct): Product {
+    return {
+      id: tesloProduct.id,
+      title: tesloProduct.title,
+      price: tesloProduct.price,
+      description: tesloProduct.description,
+      slug: tesloProduct.slug,
+      stock: tesloProduct.stock,
+      sizes: tesloProduct.sizes,
+      gender: tesloProduct.gender,
+      tags: tesloProduct.tags,
+      images: tesloProduct.images.map(
+        (image) => `${API_URL}/files/product/${image}`
+      ),
+    };
+  }
+}
