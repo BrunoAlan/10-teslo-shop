9bb247211948259ffb0396416645bcf17e1bc180
BrunoAlan <alan.brunoab@gmail.com>
2024-08-07 20:19:13 -0300

    feat: Add lazy loading for images in ProductsList component
    This commit adds lazy loading for images in the ProductsList component to improve page load performance. It also includes an update to the HomeScreen layout to use the MainLayout component, providing a consistent structure for pages. These changes enhance the user experience and code organization.

--- src/presentation/components/products/ProductsList.tsx
+++ src/presentation/components/products/ProductsList.tsx
@@ -1,12 +1,24 @@
 import { Product } from '@/src/domain/entities/product';
 import { Layout, List } from '@ui-kitten/components';
 import { ProductCard } from './ProductCard';
+import { useState } from 'react';
+import { RefreshControl } from 'react-native';
 
 interface Props {
   products: Product[];
+  fetchNextPage: () => void;
 }
 
-const ProductsList = ({ products }: Props) => {
+const ProductsList = ({ products, fetchNextPage }: Props) => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+    //sleep for 2 seconds
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setIsRefreshing(false);
+  };
+
   return (
     <List
       data={products}
@@ -14,6 +26,11 @@ const ProductsList = ({ products }: Props) => {
       keyExtractor={(item, index) => `${item.id}-${index}`}
       renderItem={({ item }) => <ProductCard product={item} />}
       ListFooterComponent={() => <Layout style={{ height: 150 }} />}
+      onEndReached={fetchNextPage}
+      onEndReachedThreshold={0.8}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onPullToRefresh} />
+      }
     />
   );
 };
