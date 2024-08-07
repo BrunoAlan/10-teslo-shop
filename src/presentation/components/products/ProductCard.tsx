4afdb0ecc0c41920c95549d34bc6838f2a3ffbb4
BrunoAlan <alan.brunoab@gmail.com>
2024-08-07 20:08:35 -0300

    feat: Add Card and FadeInImage components to ProductCard
    This commit adds the Card component from the @ui-kitten/components library and the FadeInImage component to the ProductCard in src/presentation/components/products/ProductCard.tsx. The Card component is used to wrap the product information and provide a consistent layout. The FadeInImage component is responsible for rendering the product image with a fade-in effect, enhancing the user experience. These additions improve the codebase by enhancing the visual presentation and reusability of the ProductCard component.

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { Product } from '@/src/domain/entities/product';
-import { Layout, Text } from '@ui-kitten/components';
+import { Card, Layout, Text } from '@ui-kitten/components';
+import { Image } from 'react-native';
+import { FadeInImage } from '../ui/FadeInImage';
 
 interface Props {
   product: Product;
@@ -7,8 +9,21 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <Layout>
-      <Text>{product.title}</Text>
-    </Layout>
+    <Card style={{ flex: 1, backgroundColor: '#F9F9F9', margin: 3 }}>
+      {product.images.length === 0 ? (
+        <Image
+          source={require('@/src/assets/no-product-image.png')}
+          style={{ width: '100%', height: 200 }}
+        />
+      ) : (
+        <FadeInImage
+          uri={product.images[0]}
+          style={{ flex: 1, width: '100%', height: 200 }}
+        />
+      )}
+      <Text numberOfLines={2} style={{ textAlign: 'center' }}>
+        {product.title}
+      </Text>
+    </Card>
   );
 };
