35cb42101fc3ff5e0375bffb8efa91d1c844aef9
BrunoAlan <alan.brunoab@gmail.com>
2024-08-08 08:28:44 -0300

    feat: Add lazy loading for product image in ProductScreen

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import MainLayout from '../../layouts/MainLayout';
+import { useLocalSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import { getProductById } from '@/src/actions/products/get-products-by-id';
+import { useRef } from 'react';
 const ProductScreen = () => {
+  const { Product: productId } = useLocalSearchParams();
+  const productIdRef = useRef(productId);
+
+  const { data: product, isLoading } = useQuery({
+    queryKey: ['product', productIdRef.current],
+    queryFn: () => getProductById(productId as string),
+  });
+
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (!product) {
+    return (
+      <MainLayout title='Product not found' subTitle='Product not found'>
+        <Text>Product not found</Text>
+      </MainLayout>
+    );
+  }
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <MainLayout title={product?.title} subTitle={`$ ${product?.price}`}>
+      <Text>{product?.title} </Text>
+    </MainLayout>
   );
 };
 export default ProductScreen;
-const styles = StyleSheet.create({});
