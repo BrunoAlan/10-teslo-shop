d507749370cd462fe55387361afedd9219b6b109
BrunoAlan <alan.brunoab@gmail.com>
2024-08-08 08:28:57 -0300

    feat: Add lazy loading for images in ProductCard component

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -2,6 +2,7 @@ import { Product } from '@/src/domain/entities/product';
 import { Card, Text } from '@ui-kitten/components';
 import { Image } from 'react-native';
 import { FadeInImage } from '../ui/FadeInImage';
+import { Link } from 'expo-router';
 
 interface Props {
   product: Product;
@@ -9,21 +10,23 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <Card style={{ flex: 1, backgroundColor: '#F9F9F9', margin: 3 }}>
-      {product.images.length === 0 ? (
-        <Image
-          source={require('@/src/assets/no-product-image.png')}
-          style={{ width: '100%', height: 200 }}
-        />
-      ) : (
-        <FadeInImage
-          uri={product.images[0]}
-          style={{ flex: 1, width: '100%', height: 200 }}
-        />
-      )}
-      <Text numberOfLines={2} style={{ textAlign: 'center' }}>
-        {product.title}
-      </Text>
-    </Card>
+    <Link asChild href={`/${product.id}`}>
+      <Card style={{ flex: 1, backgroundColor: '#F9F9F9', margin: 3 }}>
+        {product.images.length === 0 ? (
+          <Image
+            source={require('@/src/assets/no-product-image.png')}
+            style={{ width: '100%', height: 200 }}
+          />
+        ) : (
+          <FadeInImage
+            uri={product.images[0]}
+            style={{ flex: 1, width: '100%', height: 200 }}
+          />
+        )}
+        <Text numberOfLines={2} style={{ textAlign: 'center' }}>
+          {product.title}
+        </Text>
+      </Card>
+    </Link>
   );
 };
