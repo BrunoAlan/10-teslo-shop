4212398fff0886e4764f713c9c7cbc09d2d9ace6
BrunoAlan <alan.brunoab@gmail.com>
2024-08-10 11:37:24 -0300

    refactor: Update updateCreateProduct to handle NaN values for stock and price

--- src/actions/products/update-create-product.ts
+++ src/actions/products/update-create-product.ts
@@ -2,24 +2,28 @@ import { tesloApi } from '@/src/config/api/tesloApi';
 import { Product } from '@/src/domain/entities/product';
 import { isAxiosError } from 'axios';
 
+const prepareImages = (images: string[]) => {
+  //TODO: review files
+  return images.map((image) => image.split('/').pop());
+};
+
 export const updateCreateProduct = (product: Partial<Product>) => {
   product.stock = isNaN(Number(product.stock)) ? 0 : Number(product.stock);
   product.price = isNaN(Number(product.price)) ? 0 : Number(product.price);
 
-  if (product.id) {
+  if (product.id && product.id !== 'new') {
     return updateProduct(product);
   }
-  throw new Error('Not implemented');
+
+  return createProduct(product);
 };
 
 //TODO review if the user is coming
 const updateProduct = async (product: Partial<Product>) => {
-  console.log(product);
   const { id, images = [], ...rest } = product;
 
   try {
     const checkedImages = prepareImages(images);
-    console.log(checkedImages);
     const { data } = await tesloApi.patch(`/products/${id}`, {
       images: checkedImages,
       ...rest,
@@ -33,7 +37,20 @@ const updateProduct = async (product: Partial<Product>) => {
   }
 };
 
-const prepareImages = (images: string[]) => {
-  //TODO: review files
-  return images.map((image) => image.split('/').pop());
+export const createProduct = async (product: Partial<Product>) => {
+  const { id, images = [], ...rest } = product;
+
+  try {
+    const checkedImages = prepareImages(images);
+    const { data } = await tesloApi.post(`/products/`, {
+      images: checkedImages,
+      ...rest,
+    });
+    return data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      console.log(error.response?.data);
+    }
+    throw new Error('Error creating product');
+  }
 };
