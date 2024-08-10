5d81334f5a1dbb10f97cb27565f95dd631afa620
BrunoAlan <alan.brunoab@gmail.com>
2024-08-10 11:37:17 -0300

    feat: Update getProductById to return empty product for 'new' ID
    This commit updates the getProductById function in src/actions/products/get-products-by-id.ts to return an empty product object with default values when the ID parameter is set to 'new'. This change improves the codebase by providing a consistent behavior for creating new products and simplifying the logic in the calling code.

--- src/actions/products/get-products-by-id.ts
+++ src/actions/products/get-products-by-id.ts
@@ -1,9 +1,23 @@
 import { tesloApi } from '@/src/config/api/tesloApi';
-import { Product } from '@/src/domain/entities/product';
+import { Gender, Product } from '@/src/domain/entities/product';
 import { ProductMapper } from '@/src/infrastructure/interfaces/mappers/product.mapper';
 import { TesloProduct } from '@/src/infrastructure/interfaces/teslo-products.response';
 
+const emptyProduct: Product = {
+  id: '',
+  title: '',
+  price: 0,
+  description: '',
+  images: [],
+  slug: '',
+  gender: Gender.Unisex,
+  sizes: [],
+  stock: 0,
+  tags: [],
+};
+
 export const getProductById = async (id: string): Promise<Product> => {
+  if (id === 'new') return emptyProduct;
   try {
     const { data } = await tesloApi.get<TesloProduct>(`/products/${id}`);
     return ProductMapper.testloProductToEntity(data);
